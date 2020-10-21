1e489edf7fd4a3dec261e15b9884b7a1421bc48a
Willun <59316300+Willun@users.noreply.github.com>
2020-10-21 18:18:48 +0200

    dsadsa

--- Grupp 16/Grupp 16/Form1.Designer.cs
+++ Grupp 16/Grupp 16/Form1.Designer.cs
@@ -28,21 +28,245 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.buttonNew1 = new System.Windows.Forms.Button();
+            this.buttonSave1 = new System.Windows.Forms.Button();
+            this.buttonDelete1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxChapters = new System.Windows.Forms.ListBox();
+            this.listBoxMediaViewer = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBoxCategory = new System.Windows.Forms.ListBox();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.buttonNew2 = new System.Windows.Forms.Button();
+            this.buttonSave2 = new System.Windows.Forms.Button();
+            this.buttonDelete2 = new System.Windows.Forms.Button();
+            this.listBoxViewer = new System.Windows.Forms.ListBox();
+            this.comboBoxUpdateFrequency = new System.Windows.Forms.ComboBox();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 655);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 653);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uppdateringsfrekvens:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(708, 655);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kategori:";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(76, 678);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(203, 26);
+            this.textBoxUrl.TabIndex = 3;
+            // 
+            // buttonNew1
+            // 
+            this.buttonNew1.Location = new System.Drawing.Point(467, 768);
+            this.buttonNew1.Name = "buttonNew1";
+            this.buttonNew1.Size = new System.Drawing.Size(156, 87);
+            this.buttonNew1.TabIndex = 6;
+            this.buttonNew1.Text = "Ny...";
+            this.buttonNew1.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave1
+            // 
+            this.buttonSave1.Location = new System.Drawing.Point(642, 768);
+            this.buttonSave1.Name = "buttonSave1";
+            this.buttonSave1.Size = new System.Drawing.Size(138, 87);
+            this.buttonSave1.TabIndex = 7;
+            this.buttonSave1.Text = "Spara";
+            this.buttonSave1.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete1
+            // 
+            this.buttonDelete1.Location = new System.Drawing.Point(804, 768);
+            this.buttonDelete1.Name = "buttonDelete1";
+            this.buttonDelete1.Size = new System.Drawing.Size(143, 87);
+            this.buttonDelete1.TabIndex = 8;
+            this.buttonDelete1.Text = "Ta bort...";
+            this.buttonDelete1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 886);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Podcast";
+            // 
+            // listBoxChapters
+            // 
+            this.listBoxChapters.FormattingEnabled = true;
+            this.listBoxChapters.ItemHeight = 20;
+            this.listBoxChapters.Location = new System.Drawing.Point(48, 909);
+            this.listBoxChapters.Name = "listBoxChapters";
+            this.listBoxChapters.Size = new System.Drawing.Size(899, 304);
+            this.listBoxChapters.TabIndex = 10;
+            // 
+            // listBoxMediaViewer
+            // 
+            this.listBoxMediaViewer.FormattingEnabled = true;
+            this.listBoxMediaViewer.ItemHeight = 20;
+            this.listBoxMediaViewer.Location = new System.Drawing.Point(48, 33);
+            this.listBoxMediaViewer.Name = "listBoxMediaViewer";
+            this.listBoxMediaViewer.Size = new System.Drawing.Size(899, 544);
+            this.listBoxMediaViewer.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1005, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Kategorier:";
+            // 
+            // listBoxCategory
+            // 
+            this.listBoxCategory.FormattingEnabled = true;
+            this.listBoxCategory.ItemHeight = 20;
+            this.listBoxCategory.Location = new System.Drawing.Point(1009, 56);
+            this.listBoxCategory.Name = "listBoxCategory";
+            this.listBoxCategory.Size = new System.Drawing.Size(797, 324);
+            this.listBoxCategory.TabIndex = 13;
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(1009, 413);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(203, 26);
+            this.textBoxCategory.TabIndex = 14;
+            // 
+            // buttonNew2
+            // 
+            this.buttonNew2.Location = new System.Drawing.Point(1321, 469);
+            this.buttonNew2.Name = "buttonNew2";
+            this.buttonNew2.Size = new System.Drawing.Size(129, 87);
+            this.buttonNew2.TabIndex = 15;
+            this.buttonNew2.Text = "Ny...";
+            this.buttonNew2.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave2
+            // 
+            this.buttonSave2.Location = new System.Drawing.Point(1498, 469);
+            this.buttonSave2.Name = "buttonSave2";
+            this.buttonSave2.Size = new System.Drawing.Size(137, 87);
+            this.buttonSave2.TabIndex = 16;
+            this.buttonSave2.Text = "Spara";
+            this.buttonSave2.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete2
+            // 
+            this.buttonDelete2.Location = new System.Drawing.Point(1677, 469);
+            this.buttonDelete2.Name = "buttonDelete2";
+            this.buttonDelete2.Size = new System.Drawing.Size(129, 87);
+            this.buttonDelete2.TabIndex = 17;
+            this.buttonDelete2.Text = "Ta bort...";
+            this.buttonDelete2.UseVisualStyleBackColor = true;
+            // 
+            // listBoxViewer
+            // 
+            this.listBoxViewer.FormattingEnabled = true;
+            this.listBoxViewer.ItemHeight = 20;
+            this.listBoxViewer.Location = new System.Drawing.Point(1009, 584);
+            this.listBoxViewer.Name = "listBoxViewer";
+            this.listBoxViewer.Size = new System.Drawing.Size(797, 624);
+            this.listBoxViewer.TabIndex = 18;
+            // 
+            // comboBoxUpdateFrequency
+            // 
+            this.comboBoxUpdateFrequency.FormattingEnabled = true;
+            this.comboBoxUpdateFrequency.Location = new System.Drawing.Point(380, 676);
+            this.comboBoxUpdateFrequency.Name = "comboBoxUpdateFrequency";
+            this.comboBoxUpdateFrequency.Size = new System.Drawing.Size(199, 28);
+            this.comboBoxUpdateFrequency.TabIndex = 19;
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Location = new System.Drawing.Point(712, 678);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(235, 28);
+            this.comboBoxCategory.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1852, 1235);
+            this.Controls.Add(this.comboBoxCategory);
+            this.Controls.Add(this.comboBoxUpdateFrequency);
+            this.Controls.Add(this.listBoxViewer);
+            this.Controls.Add(this.buttonDelete2);
+            this.Controls.Add(this.buttonSave2);
+            this.Controls.Add(this.buttonNew2);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.listBoxCategory);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBoxMediaViewer);
+            this.Controls.Add(this.listBoxChapters);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonDelete1);
+            this.Controls.Add(this.buttonSave1);
+            this.Controls.Add(this.buttonNew1);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Button buttonNew1;
+        private System.Windows.Forms.Button buttonSave1;
+        private System.Windows.Forms.Button buttonDelete1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBoxChapters;
+        private System.Windows.Forms.ListBox listBoxMediaViewer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBoxCategory;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.Button buttonNew2;
+        private System.Windows.Forms.Button buttonSave2;
+        private System.Windows.Forms.Button buttonDelete2;
+        private System.Windows.Forms.ListBox listBoxViewer;
+        private System.Windows.Forms.ComboBox comboBoxUpdateFrequency;
+        private System.Windows.Forms.ComboBox comboBoxCategory;
     }
 }
 
