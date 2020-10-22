b036946fe028ef21b62f346834e7002499556574
Willun <59316300+Willun@users.noreply.github.com>
2020-10-22 17:03:28 +0200

    sdadsadsa

--- Grupp 16/DataAccesLayer/DataManager.cs
+++ Grupp 16/DataAccesLayer/DataManager.cs
@@ -6,26 +6,26 @@ namespace DataAccesLayer
 {
     public class DataManager
     {
-        public void Serialize(List<Podcast> personList)
+        public void Serialize(List<Podcast> podcastList)
         {
-            XmlSerializer xmlSerializer = new XmlSerializer(personList.GetType());
+            XmlSerializer xmlSerializer = new XmlSerializer(podcastList.GetType());
             using (FileStream outFile = new FileStream("Podcast.xml", FileMode.Create,
                 FileAccess.Write))
             {
-                xmlSerializer.Serialize(outFile, personList);
+                xmlSerializer.Serialize(outFile, podcastList);
             }
         }
 
-        public List<Person> Deserialize()
+        public List<Podcast> Deserialize()
         {
-            List<Person> listOfPersonsToBeReturned;
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Person>));
+            List<Podcast> listOfPodcastsToBeReturned;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Podcast>));
             using (FileStream inFile = new FileStream("Podcast.xml", FileMode.Open,
                 FileAccess.Read))
             {
-                listOfPersonsToBeReturned = (List<Person>)xmlSerializer.Deserialize(inFile);
+                listOfPodcastsToBeReturned = (List<Podcast>)xmlSerializer.Deserialize(inFile);
             }
-            return listOfPersonsToBeReturned;
+            return listOfPodcastsToBeReturned;
         }
     }
 }
