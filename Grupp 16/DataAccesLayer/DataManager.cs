73833bd00005befbd28d778e527ef5ecab506dcd
Willun <59316300+Willun@users.noreply.github.com>
2020-10-22 16:58:37 +0200

    dsadsa

--- Grupp 16/DataAccesLayer/DataManager.cs
+++ Grupp 16/DataAccesLayer/DataManager.cs
@@ -1,7 +1,31 @@
-﻿namespace DataAccesLayer
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DataAccesLayer
 {
     public class DataManager
     {
+        public void Serialize(List<Podcast> personList)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(personList.GetType());
+            using (FileStream outFile = new FileStream("Podcast.xml", FileMode.Create,
+                FileAccess.Write))
+            {
+                xmlSerializer.Serialize(outFile, personList);
+            }
+        }
 
+        public List<Person> Deserialize()
+        {
+            List<Person> listOfPersonsToBeReturned;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Person>));
+            using (FileStream inFile = new FileStream("Podcast.xml", FileMode.Open,
+                FileAccess.Read))
+            {
+                listOfPersonsToBeReturned = (List<Person>)xmlSerializer.Deserialize(inFile);
+            }
+            return listOfPersonsToBeReturned;
+        }
     }
 }
--- Grupp 16/Models/Podcast.cs
+++ Grupp 16/Models/Podcast.cs
@@ -1,6 +1,18 @@
 ﻿namespace Models
 {
-    public class Podcast
+    public abstract class Podcast
     {
+        public string Avsnitt { get; set; }
+        public string Namn { get; set; }
+        public string Frekvens { get; set; }
+        public string Kategori { get; set; }
+
+        public Podcast(string avsnitt, string namn, string frekvens, string kategori)
+        {
+            Avsnitt = avsnitt;
+            Namn = namn;
+            Frekvens = frekvens;
+            Kategori = kategori;
+        }
     }
 }
