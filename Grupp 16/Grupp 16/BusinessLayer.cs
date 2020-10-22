3f3988b9dc92dca9dcb1747c7e1b30de7eae9c04
Willun <59316300+Willun@users.noreply.github.com>
2020-10-22 12:50:50 +0200

    laddade ner 2 packet

--- Grupp 16/Grupp 16/BusinessLayer.cs
+++ Grupp 16/Grupp 16/BusinessLayer.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Grupp_16
 {
     class BusinessLayer
     {
+        public void hej()
+        {
+            Console.WriteLine("hej");
+        }
     }
 }
